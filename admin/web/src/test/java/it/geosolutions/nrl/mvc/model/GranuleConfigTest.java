15fd996777aac04fc4e4a651b943acc9cdc7fe87
Lorenzo Natali <lorenzo.natali@geo-solutions.it>
2013-05-21 16:19:05 +0200

    configuration for granule

---
+++ admin/web/src/test/java/it/geosolutions/nrl/mvc/model/GranuleConfigTest.java
@@ -0,0 +1,28 @@
+package it.geosolutions.nrl.mvc.model;
+
+import static org.junit.Assert.*;
+import it.geosolutions.nrl.mvc.model.statistics.GranuleConfig;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GranuleConfigTest {
+	private String infoFile;
+	@Before
+	public void getFile(){
+		infoFile = getClass().getClassLoader().getResource("info.xml").getFile();
+	}
+	
+	@Test
+	public void TestGranules(){
+		GranuleConfig gf = new GranuleConfig();
+		gf.setInfoFile(infoFile);
+		String[] ts = gf.getTimeSeries();
+		assertNotNull(ts);
+    	System.out.println(ts[0]);
+
+	}
+
+}
