a015869d868190b177e1e5b4ca8c84927c729d4b
Lorenzo Pini <lore.pini@tin.it>
2016-01-25 15:38:54 +0100

    Backup wars

--- updates/2016/01_check_install.sh
+++ updates/2016/01_check_install.sh
@@ -19,9 +19,7 @@ function check_exists(){
 }
 
 # TODAY
-TODAY = "date +%Y%m%d"
-
-
+TODAY=$(date +%Y%m%d)
 
 # Check startup scripts
 check_exists "/etc/init.d/geobatch"
@@ -41,8 +39,27 @@ fi
 
 # TODO chkconfig httpd on
 
+# Check tomcat installs
+check_exists "/opt/tomcat_gui/webapps/MapStore.war"
+check_exists "/opt/tomcat_gui/webapps/admin.war"
+check_exists "/opt/tomcat_geobatch/webapps/geobatch.war"
+check_exists "/opt/tomcat_geoserver/webapps/geoserver.war"
+check_exists "/opt/tomcat_geostore/webapps/geostore.war"
+
 # Check tomcat logrotate
 check_exists "/etc/logrotate.d/tomcat"
+
+# Backup wars
+df -h
+du -h /opt/tomcat_* -s -c
+read -rsp $'Press enter to backup existing WARS...\n'
+eval "mkdir -p bkp_wars_${TODAY}/{gui,geobatch,geoserver,geostore}"
+
+cp -rv "/opt/tomcat_gui/webapps" "./bkp_wars_${TODAY}/gui"
+cp -rv "/opt/tomcat_geobatch/webapps" "./bkp_wars_${TODAY}/geobatch"
+cp -rv "/opt/tomcat_geoserver/webapps" "./bkp_wars_${TODAY}/geoserver"
+cp -rv "/opt/tomcat_geostore/webapps" "./bkp_wars_${TODAY}/geostore"
+
 
 # TODO: Update logrotate
 
